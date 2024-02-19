11a3a0334cef55c21287380ca3e489e95f08737a
FrancescoPB <146762060+TesEzzo@users.noreply.github.com>
2024-02-19 17:50:19 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,19 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import { useSelector } from "react-redux"
-import Login from "./pages/Login"
+import { Navigate, Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import Home from "./pages/Home";
 import DefaultLayout from "./layout/DefaultLayout";
 
-const ProtectedRoute = ({ children }) => {
+const ProtectedRoute = ({ children, identity }) => {
   const auth = useSelector((state) => state.auth);
 
-  if (auth.token === null) {
-    return <Navigate to="/login" />
+  if (auth.token === null || auth.identity !== identity) {
+    return <Navigate to="/login" />;
   }
 
   return children;
-}
+};
 
 const App = () => {
   return (
@@ -22,15 +22,34 @@ const App = () => {
         <Route path="/" element={<DefaultLayout />}>
           <Route path="" element={<Home />} />
           <Route path="login" element={<Login />} />
-          <Route path="profile" element={
-            <ProtectedRoute>
-              <Profile />
+        </Route>
+        <Route
+          path="/services"
+          element={
+            <ProtectedRoute identity="user">
+              <DefaultLayout />
             </ProtectedRoute>
-          } />
+          }
+        >
+          <Route path="" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+        <Route
+          path="/club"
+          element={
+            <ProtectedRoute identity="club">
+              <DefaultLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="profile" element={<Profile />} />
         </Route>
       </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
