753616028acc0398da581b6f81a2c10c1fce8dce
Giuseppe <g.mattera@yahoo.com>
2024-03-22 00:43:55 +0100

    need_only_date_for_create_event

--- client/src/components/Events.jsx
+++ client/src/components/Events.jsx
@@ -7,6 +7,7 @@ const Events = () => {
     const [newEvent, setNewEvent] = useState(false);
     const [clubData, setClubData] = useState(null);
     const [space, setSpace] = useState(null);
+    const [maxPlayers, setMaxPlayers] = useState([]);
     const [eventInfo, setEventInfo] = useState({
         club: "",
         space: "",
@@ -46,6 +47,21 @@ const Events = () => {
         event.preventDefault();
     };
 
+    const handleMaxPlayers = (event) => {
+        const maxPlayers = event.target.value;
+        if (eventInfo.players_max_count === null) {
+            setEventInfo((actualValues) => ({
+                ...actualValues,
+                  players_max_count: maxPlayers
+            }));
+        } else {
+            setEventInfo((actualValues) => ({
+                ...actualValues,
+                  players_max_count: null
+            }));
+        }
+    }
+
     useEffect(() => {
         setTimeout(() => {
             const extractedSport = space.find((item) => item._id === eventInfo.space );
@@ -53,7 +69,8 @@ const Events = () => {
                 ...actualValues,
                 sport: extractedSport ? extractedSport.sport._id : ""
             }));
-        }, 1000);
+            setMaxPlayers(extractedSport ? extractedSport.sport.players : null);
+        }, 500);
     }, [eventInfo.space]);
 
     useEffect(() => {
@@ -94,7 +111,8 @@ const Events = () => {
     const prova = () => {
         // console.log(clubData);
         console.log(eventInfo);
-        // console.log(space);
+        console.log(space);
+        console.log(maxPlayers);
     }
 
 
@@ -115,7 +133,7 @@ const Events = () => {
                         <div className="flex flex-col justify-center items-center border-2 rounded-3xl border-none max-w-[500px] max-h shadow-md bg-white m-2 p-2 gap-5">
                             <form onSubmit={handleSubmit} className="max-w-md mx-auto">
                                 {/* SCEGLIERE LO SPORT NON MI SERVE PERCHE' SCEGLIENDO LO SPACE SCELGO AUTOMATICAMENTE ANCHE L'ID DELLO SPORT */}
-                                {/* <div className="relative z-0 w-full mb-5 group">
+                                {/* <div className="relative z-0 w-full mb-5">
                                     {
                                         clubData.info.sports.length > 0 
                                         && 
@@ -136,7 +154,7 @@ const Events = () => {
                                         </div>
                                     }
                                 </div> */}
-                                <div className="relative z-0 w-full mb-5 group">
+                                <div className="relative z-0 w-full mb-5">
                                     <h4>scegli lo sport ed il relativo campo</h4>
                                     {
                                         space.length > 0
@@ -159,29 +177,53 @@ const Events = () => {
                                     }
                                 </div>
                                 <div className="grid md:grid-cols-2 md:gap-6">
-                                    <div className="relative z-0 w-full mb-5 group">
-                                        <input type="text" name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                                        <label htmlFor="floating_first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Numero Giocatori</label>
+                                    <div className="relative z-0 w-full mb-5">
+                                        {
+                                            eventInfo.sport !== "" ?
+                                            <div>
+                                                <h6>Numero giocatori</h6>
+                                                {
+                                                    maxPlayers.length > 0
+                                                    &&
+                                                    maxPlayers.map((item, index) => (
+                                                        <button key={index} value={item} 
+                                                            className={`${eventInfo.players_max_count == item ? 
+                                                            "text-black border border-green-700 bg-[#89EDAF] hover:bg-[#89EDAF]/80 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-1 text-center me-2 mb-2" 
+                                                            : 
+                                                            "text-gray-900 border border-gray-800 hover:bg-[#EBEBEB] focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-1 text-center me-2 mb-2"}`}
+                                                            onClick={handleMaxPlayers}
+                                                            >
+                                                            {item}
+                                                        </button>
+                                                    ))
+                                                }
+                                            </div>
+                                            :
+                                            <div>
+                                                <h6>Numero giocatori</h6>
+                                                <p>Seleziona prima il campo</p>
+                                            </div>
+                                        }
                                     </div>
-                                    <div className="relative z-0 w-full mb-5 group">
+                                    <div className="relative z-0 w-full mb-5">
                                         <input type="text" name="floating_last_name" id="floating_last_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                                         <label htmlFor="floating_last_name" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Giocatori</label>
                                     </div>
                                 </div>
                                 <div className="grid md:grid-cols-2 md:gap-6">
-                                    <div className="relative z-0 w-full mb-5 group">
+                                    <div className="relative z-0 w-full mb-5">
                                         <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                                         <label htmlFor="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Ospiti</label>
                                     </div>
-                                    <div className="relative z-0 w-full mb-5 group">
+                                    <div className="relative z-0 w-full mb-5">
                                         <input type="text" name="floating_company" id="floating_company" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                                         <label htmlFor="floating_company" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Data Partita</label>
                                     </div>
-                                    <div className="relative z-0 w-full mb-5 group">
+                                    <div className="relative z-0 w-full mb-5">
                                         <input type="text" name="floating_company" id="Pi" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                                         <label htmlFor="floating_company" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">inizio Partita</label>
                                     </div>
-                                    <div className="relative z-0 w-full mb-5 group">
+                                    <div className="relative z-0 w-full mb-5">
                                         <input type="text" name="floating_company" id="PF" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                                         <label htmlFor="floating_company" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Fine Partita</label>
                                     </div>
