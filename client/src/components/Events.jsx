32be9bc7e21d404b5c7d7027a9b6aac53f54a3ef
AngeloRaffaele <ralf9997@gmail.com>
2024-03-27 10:37:09 +0100

    pull

--- client/src/components/Events.jsx
+++ client/src/components/Events.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
+i
 
 const Events = () => {
     const [newEvent, setNewEvent] = useState(false);
 
-    const clubId = props.match.params.clubId;
 
     const handleNewEvent = () => {
         if (newEvent) {
@@ -12,7 +12,7 @@ const Events = () => {
             setNewEvent(true);
         }
     };
-    
+
 
     return (
         <>
@@ -31,12 +31,16 @@ const Events = () => {
                         <div className="flex flex-col justify-center items-center border-2 rounded-3xl border-none max-w-[500px] max-h shadow-md bg-white m-2 p-2 gap-5">
                             <form className="max-w-md mx-auto">
                                 <div className="relative z-0 w-full mb-5 group">
-                                    <select type="text" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                                    <input type="text" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                                     <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Club</label>
                                 </div>
                                 <div className="relative z-0 w-full mb-5 group">
-                                    <input type="password" name="floating_password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                                    <label htmlFor="floating_password" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Sport</label>
+                                    <select id="underline_select" className="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none focus:outline-none focus:ring-0 focus:border-gray-200 peer">
+                                        <option selected>Sport</option>
+                                        <option value="US">Calcio</option>
+                                        <option value="CA">Padel</option>
+                                        <option value="FR">Tennis</option>
+                                    </select>
                                 </div>
                                 <div className="relative z-0 w-full mb-5 group">
                                     <input type="password" name="repeat_password" id="floating_repeat_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
